0ae97f6e04b76477179d10a2a61762f9f8075be5
yangna <ynparis@.163.com>
2020-05-09 17:09:57 +0800

    Add detail player.

--- app/src/main/java/com/demos/vedioplaybygsyplayer/DetailPlayerActivity.java
+++ app/src/main/java/com/demos/vedioplaybygsyplayer/DetailPlayerActivity.java
@@ -26,7 +26,9 @@ public class DetailPlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_player);
         detailPlayer = findViewById(R.id.empty_player);
-        String source1 = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4";
+//        String source1 = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4";
+        GSYVideoManager.instance().enableRawPlay(getApplicationContext());
+        String source1 = "rtsp://61.138.102.214:619/1101:admin:paxy2019@10.1.29.53:554:0:1";
         detailPlayer.setUp(source1, true, "测试视频");
         //设置旋转
         orientationUtils = new OrientationUtils(this, detailPlayer);
@@ -83,7 +85,8 @@ public class DetailPlayerActivity extends AppCompatActivity {
      */
     public void switchPixel(View view) {
         detailPlayer.onVideoPause();
-        String source1 = "http://7xjmzj.com1.z0.glb.clouddn.com/20171026175005_JObCxCE2.mp4";
+//        String source1 = "http://7xjmzj.com1.z0.glb.clouddn.com/20171026175005_JObCxCE2.mp4";
+        String source1 = "rtsp://61.138.102.214:619/1101:admin:paxy2019@10.1.29.9:554:0:0";
         detailPlayer.setUp(source1, true, "测试视频2");
         detailPlayer.onVideoReset();
         detailPlayer.startPlayLogic();
